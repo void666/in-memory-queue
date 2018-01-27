4bc713037543b240525b30fd2d26e597d3def08a
Sushim Mukul Dutta <sushimmdutta@gmail.com>
2018-01-28 00:51:20 +0530

    - remove test code

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const messageService = require('./services/message.service');
 const consumerService = require('./services/consumer.service');
 const topicService = require('./services/topic.service');
 
-const IMQInterface = {
+module.exports = {
     createConsumer: consumerService.createConsumer,
     createMessage: messageService.createMessage,
     createTopic: topicService.createTopic,
     QUEUE_CONFIGURATION: queueService.QueueConfiguration,
     getStatus: queueService.getStatus
 };
-
-module.exports = IMQInterface;
